a0f3bf4b4aac0792051561931c5e3a2196218e72
sumanth41518 <116655462+sumanth41518@users.noreply.github.com>
2024-02-22 13:55:41 +0530

    Update javas2.js

--- javas2.js
+++ javas2.js
@@ -3,8 +3,7 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
     
-    // Here you would typically handle the login process, such as sending the email and password to a server.
-    // For the sake of this example, let's just log them to the console.
+
     console.log('Email:', email);
     console.log('Password:', password);
   });
